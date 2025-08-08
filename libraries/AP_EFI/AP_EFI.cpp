f563f3b7de0ea7109426864b5fe03ad365a1c1fc
Bob Long <robertlong13@gmail.com>
2025-08-08 13:19:27 +1000

    AP_EFI: send CHT2 and EGT2 over Mavlink

--- libraries/AP_EFI/AP_EFI.cpp
+++ libraries/AP_EFI/AP_EFI.cpp
@@ -316,6 +316,10 @@ void AP_EFI::send_mavlink_status(mavlink_channel_t chan)
         ignition_voltage,
         fuel_pressure
         );
+#if HAL_GCS_ENABLED
+    gcs().send_named_float("CHT2", KELVIN_TO_C(state.cylinder_status.cylinder_head_temperature2));
+    gcs().send_named_float("EGT2", KELVIN_TO_C(state.cylinder_status.exhaust_gas_temperature2));
+#endif
 }
 
 // get a copy of state structure
