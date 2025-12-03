CarbonixUAV/carbopilot_V2

AP_ESC_Telem: add count and error_count handlers

libraries/AP_ESC_Telem/AP_ESC_Telem.cpp
@@ -326,6 +326,26 @@ bool AP_ESC_Telem::get_consumption_mah(uint8_t esc_index, float& consumption_mah
     return true;
 }
 
+// get an individual ESC's packet count if available, returns true on success
+bool AP_ESC_Telem::get_count(uint8_t esc_index, uint16_t& count) const
+{
+    if (esc_index >= ESC_TELEM_MAX_ESCS) {
+        return false;
+    }
+    count = _telem_data[esc_index].count;
+    return true;
+}
+
+// get an individual ESC's error count if available, returns true on success
+bool AP_ESC_Telem::get_error_count(uint8_t esc_index, uint16_t& error_count) const
+{
+    if (esc_index >= ESC_TELEM_MAX_ESCS) {
+        return false;
+    }
+    error_count = _telem_data[esc_index].error_count;
+    return true;
+}
+
 // get an individual ESC's usage time in seconds if available, returns true on success
 bool AP_ESC_Telem::get_usage_seconds(uint8_t esc_index, uint32_t& usage_s) const
 {
@@ -562,6 +582,9 @@ void AP_ESC_Telem::update_telem_data(const uint8_t esc_index, const AP_ESC_Telem
     if (data_mask & AP_ESC_Telem_Backend::TelemetryType::USAGE) {
         telemdata.usage_s = new_data.usage_s;
     }
+    if (data_mask & AP_ESC_Telem_Backend::TelemetryType::ERROR_COUNT) {
+        telemdata.error_count = new_data.error_count;
+    }
 
 #if AP_EXTENDED_ESC_TELEM_ENABLED
     if (data_mask & AP_ESC_Telem_Backend::TelemetryType::INPUT_DUTY) {
@@ -581,6 +604,15 @@ void AP_ESC_Telem::update_telem_data(const uint8_t esc_index, const AP_ESC_Telem
     telemdata.count++;
     telemdata.types |= data_mask;
     telemdata.last_update_ms = AP_HAL::millis();
+}
+
+// callback to increment the error count in the frontend, should be called by the driver when an error occurs
+void AP_ESC_Telem::increment_error_count(const uint8_t esc_index)
+{
+    if (esc_index >= ESC_TELEM_MAX_ESCS) {
+        return;
+    }
+    _telem_data[esc_index].error_count++;
 }
 
 // record an update to the RPM together with timestamp, this allows the notch values to be slewed

CarbonixUAV/cxpilot-core

libraries/AP_ESC_Telem/AP_ESC_Telem.cpp
@@ -326,6 +326,26 @@ bool AP_ESC_Telem::get_consumption_mah(uint8_t esc_index, float& consumption_mah
     return true;
 }
 
+// get an individual ESC's packet count if available, returns true on success
+bool AP_ESC_Telem::get_count(uint8_t esc_index, uint16_t& count) const
+{
+    if (esc_index >= ESC_TELEM_MAX_ESCS) {
+        return false;
+    }
+    count = _telem_data[esc_index].count;
+    return true;
+}
+
+// get an individual ESC's error count if available, returns true on success
+bool AP_ESC_Telem::get_error_count(uint8_t esc_index, uint32_t& error_count) const
+{
+    if (esc_index >= ESC_TELEM_MAX_ESCS) {
+        return false;
+    }
+    error_count = _telem_data[esc_index].error_count;
+    return true;
+}
+
 // get an individual ESC's usage time in seconds if available, returns true on success
 bool AP_ESC_Telem::get_usage_seconds(uint8_t esc_index, uint32_t& usage_s) const
 {
@@ -565,6 +585,9 @@ void AP_ESC_Telem::update_telem_data(const uint8_t esc_index, const AP_ESC_Telem
     if (data_mask & AP_ESC_Telem_Backend::TelemetryType::USAGE) {
         telemdata.usage_s = new_data.usage_s;
     }
+    if (data_mask & AP_ESC_Telem_Backend::TelemetryType::ERROR_COUNT) {
+        telemdata.error_count = new_data.error_count;
+    }
 
 #if AP_EXTENDED_ESC_TELEM_ENABLED
     if (data_mask & AP_ESC_Telem_Backend::TelemetryType::INPUT_DUTY) {
@@ -593,6 +616,15 @@ void AP_ESC_Telem::update_telem_data(const uint8_t esc_index, const AP_ESC_Telem
     telemdata.count++;
     telemdata.types |= data_mask;
     telemdata.last_update_ms = AP_HAL::millis();
+}
+
+// callback to increment the error count in the frontend, should be called by the driver when an error occurs
+void AP_ESC_Telem::increment_error_count(const uint8_t esc_index, const uint32_t amount)
+{
+    if (esc_index >= ESC_TELEM_MAX_ESCS) {
+        return;
+    }
+    _telem_data[esc_index].error_count += amount;
 }
 
 // record an update to the RPM together with timestamp, this allows the notch values to be slewed