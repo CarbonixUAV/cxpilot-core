Hurannn/Ardupilot

Plane: improve POSITION1 velocity control

ArduPlane/quadplane.cpp
@@ -2639,6 +2639,8 @@ void QuadPlane::vtol_position_controller(void)
 
         // use input shaping and abide by accel and jerk limits
         pos_control->input_vel_accel_NE_cm(target_speed_xy_cms, target_accel_cms);
+        // During POS1, we only want to control velocity and acceleration
+        pos_control->stop_pos_NE_stabilisation();
 
         // run horizontal velocity controller
         run_xy_controller(MAX(target_accel, transition_decel)*1.5);

CarbonixUAV/cxpilot-core

(cherry picked from commit 9776fdf5001857e5a03d57409b2b27ebe4a69a3b)
Adapted for 4.6:
stop_pos_NE_stabilisation() -> stop_pos_xy_stabilisation()

ArduPlane/quadplane.cpp
@@ -2639,6 +2639,8 @@ void QuadPlane::vtol_position_controller(void)
 
         // use input shaping and abide by accel and jerk limits
         pos_control->input_vel_accel_xy(target_speed_xy_cms, target_accel_cms);
+        // During POS1, we only want to control velocity and acceleration
+        pos_control->stop_pos_xy_stabilisation();
 
         // run horizontal velocity controller
         run_xy_controller(MAX(target_accel, transition_decel)*1.5);