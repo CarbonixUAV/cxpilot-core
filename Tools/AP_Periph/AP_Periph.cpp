3ad94e3390eb24433793d4d146f4fc8eab543ce3
Jono453 <jonathan.loong9328@gmail.com>
2026-08-06 09:54:03 +1000

    feat: test AP_Periph.cpp changes for tmp1075 test

--- Tools/AP_Periph/AP_Periph.cpp
+++ Tools/AP_Periph/AP_Periph.cpp
@@ -441,6 +441,46 @@ void AP_Periph_FW::update()
 #endif
                    );
 #endif
+#if AP_TEMPERATURE_SENSOR_ENABLED
+        {
+            float temp1, temp2, temp3, temp4;
+            const bool h1 = temperature_sensor.get_temperature(temp1, 0);
+            const bool h2 = temperature_sensor.get_temperature(temp2, 1);
+            const bool h3 = temperature_sensor.get_temperature(temp3, 2);
+            const bool h4 = temperature_sensor.get_temperature(temp4, 3);
+            can_printf("TMP1075: 8V2=%.1f 5V=%.1f 3V3=%.1f BRD=%.1f",
+                       h1 ? temp1 : -1,
+                       h2 ? temp2 : -1,
+                       h3 ? temp3 : -1,
+                       h4 ? temp4 : -1);
+        }
+#endif
+#if defined(HAL_GPIO_PIN_8V2_TEMP_ALERT) || defined(HAL_GPIO_PIN_5V_TEMP_ALERT) || \
+    defined(HAL_GPIO_PIN_3V3_TEMP_ALERT) || defined(HAL_GPIO_PIN_BRD_TEMP_ALERT)
+        // active-low: 0 = alert asserted (TMP1075 LLIM/HLIM comparator tripped), 1 = normal
+        can_printf("TEMP_ALERT: 8V2=%d 5V=%d 3V3=%d BRD=%d",
+#ifdef HAL_GPIO_PIN_8V2_TEMP_ALERT
+                   (int)palReadLine(HAL_GPIO_PIN_8V2_TEMP_ALERT),
+#else
+                   -1,
+#endif
+#ifdef HAL_GPIO_PIN_5V_TEMP_ALERT
+                   (int)palReadLine(HAL_GPIO_PIN_5V_TEMP_ALERT),
+#else
+                   -1,
+#endif
+#ifdef HAL_GPIO_PIN_3V3_TEMP_ALERT
+                   (int)palReadLine(HAL_GPIO_PIN_3V3_TEMP_ALERT),
+#else
+                   -1,
+#endif
+#ifdef HAL_GPIO_PIN_BRD_TEMP_ALERT
+                   (int)palReadLine(HAL_GPIO_PIN_BRD_TEMP_ALERT)
+#else
+                   -1
+#endif
+                   );
+#endif
 #if 0
 #ifdef HAL_PERIPH_ENABLE_GPS
         hal.serial(0)->printf("GPS status: %u\n", (unsigned)gps.status());
